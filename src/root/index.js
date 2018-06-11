ee9946fc501678211a204782c4398aa2ed5d60a2
erifranck <erifranckjose@gmail.com>
2018-06-11 18:02:16 -0500

    insert global styles

--- src/root/index.js
+++ src/root/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import {Root} from 'root/containers'
 import configureStore from 'root/store'
+import 'root/styles/global-styles'
 
 const store = configureStore()
 
-ReactDOM.render(<Root store={store} />, document.getElementById('root'));
+ReactDOM.render(<Root store={store} />, document.getElementById('root'))
