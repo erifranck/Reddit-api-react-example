2ac7cb089ee5d348b8c571331059246b1d18156a
erifranck <erifranckjose@gmail.com>
2018-06-11 19:37:31 -0500

    insert theme provider

--- src/root/containers/Root.jsx
+++ src/root/containers/Root.jsx
@@ -3,18 +3,22 @@ import PropTypes from 'prop-types'
 import {Provider} from 'react-redux'
 import {HashRouter} from 'react-router-dom'
 import {renderRoutes} from 'react-router-config'
+import {ThemeProvider} from 'styled-components'
 
 // custom import
 import routes from 'root/routes'
 
-export const Root = ({store}) => (
+export const Root = ({store, theme = { }}) => (
   <Provider store={store}>
-    <HashRouter>
-      { renderRoutes(routes) }
-    </HashRouter>
+    <ThemeProvider theme={theme}>
+      <HashRouter>
+        { renderRoutes(routes) }
+      </HashRouter>
+    </ThemeProvider>
   </Provider>
 )
 
 Root.propTypes = {
-  store: PropTypes.object
+  store: PropTypes.object,
+  theme: PropTypes.object
 }
