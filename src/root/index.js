4fd114e28988e519b40c3962411107f70e037efb
erifranck <erifranckjose@gmail.com>
2018-06-11 19:36:13 -0500

    insert theme

--- src/root/index.js
+++ src/root/index.js
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Root} from 'root/containers'
 import configureStore from 'root/store'
+import theme from 'root/styles/theme'
 import 'root/styles/global-styles'
 
 const store = configureStore()
 
-ReactDOM.render(<Root store={store} />, document.getElementById('root'))
+ReactDOM.render(<Root store={store} theme={theme} />, document.getElementById('root'))
