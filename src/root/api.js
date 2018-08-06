f0bbf5e51004992ba7d0415ccfbcd3c63d5391f3
erifranck <erifranckjose@gmail.com>
2018-08-05 22:33:47 -0500

    update elemets

---
+++ src/root/api.js
@@ -0,0 +1,7 @@
+import {create} from 'apisauce'
+
+export const api = create({
+  baseURL: 'https://www.reddit.com/api/v1/',
+  headers: {
+  }
+})
--- src/root/saga.js
+++ src/root/saga.js
@@ -1,5 +1,13 @@
-function * saga() {
+import {takeLatest} from 'redux-saga/effects'
+import {
+  get as getCategories
+} from 'categories/saga'
+import {
+  types
+} from 'categories/reducer'
 
+function * saga() {
+  yield takeLatest(types.GET_CATEGORIES, getCategories)
 }
 
 export default saga
