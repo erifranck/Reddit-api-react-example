061da60d2252a745df902c5bc84c8a97f3c77399
erifranck <erifranckjose@gmail.com>
2018-08-22 23:01:06 -0500

    export with redux connect

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,14 @@
 import {App} from 'main/containers'
 import {Home} from 'main/styles'
+import {connect} from 'react-redux'
+import actions from 'categories/reducer'
 
 export const AppPage = App
-export const HomePage = Home
+export const HomePage = connect(
+  (state) => ( {
+    subreddits: state.categories.subreddits
+  }),
+  {
+    getSubreddit: actions.getSubreddit
+  }
+)(Home)
