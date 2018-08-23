d388e128d137accb1f6b2ca85d8725f6a1f3a07a
erifranck <erifranckjose@gmail.com>
2018-08-22 23:00:51 -0500

    test request

--- src/main/containers/Home.jsx
+++ src/main/containers/Home.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {Button, Header} from 'common/styles'
 import Background from 'main/images/bg.png'
 
-export const Home = (props) => (
-  <React.Fragment>
-    <Header />
-    <div className={props.className}>
-      <div>
-        <img src={Background} alt='' />
-      </div>
-      <Button label='Login In' />
-    </div>
-  </React.Fragment>
-)
+export class Home extends Component {
+  componentDidMount() {
+    this.props.getSubreddit('overwatch')
+  }
+  render () {
+    console.log(this.props.subreddits)
+    return (
+      <React.Fragment>
+        <Header />
+        <div className={this.props.className}>
+          <div>
+            <img src={Background} alt='' />
+          </div>
+          <Button label='Login In' />
+        </div>
+      </React.Fragment>
+    )
+  }
+}
 
 Home.propTypes = {
   className: PropTypes.string
