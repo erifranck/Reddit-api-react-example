668fee0a76e229bb01e9f25d4fc601c6dd1f5a4f
erifranck <erifranckjose@gmail.com>
2018-08-24 12:14:26 -0500

    change root styles

--- src/root/styles/global-styles.js
+++ src/root/styles/global-styles.js
@@ -1,33 +1,68 @@
 import { injectGlobal } from 'styled-components'
 import theme from './theme'
 
+const calculateFont = (scale) => theme.fonts.baseSize * scale
 /* eslint no-unused-expressions: 0 */
 injectGlobal`
-  html,
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed, 
+  figure, figcaption, footer, header, hgroup, 
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
   body {
-    height: 100%;
-    width: 100%;
-    position: absolute;
-    background: ${theme.palette.white} );
+    line-height: 1;
   }
-
-  body {
-    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  ol, ul {
+    list-style: none;
   }
-
-  body.fontLoaded {
-    font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  blockquote, q {
+    quotes: none;
   }
-
-  #app {
-    background-color: #fafafa;
-    min-height: 100%;
-    min-width: 100%;
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
   }
-
-  p,
-  label {
-    font-family: Georgia, Times, 'Times New Roman', serif;
-    line-height: 1.5em;
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  h1 {
+    font-size: ${calculateFont(3)}px;
+  }
+  h2 {
+    font-size: ${calculateFont(2.5)}px;
+  }
+  h3 {
+    font-size: ${calculateFont(2)}px;
+  }
+  h4 {
+    font-size: ${calculateFont(1.5)}px;
+  }
+  h5 {
+    font-size: ${calculateFont(1.25)}px;
+  }
+  h6 {
+    font-size: ${calculateFont(1)}px;
   }
 `
--- src/root/styles/theme.js
+++ src/root/styles/theme.js
@@ -2,13 +2,15 @@ const colorpalette = {
   aqua: '#20A39E',
   red: '#931621',
   sky: '#2274A5',
+  yellow: '#FFC70E',
   white: '#E0E4EE',
   charcoal: '#061426'
 
 }
 
 const fontRules = {
-
+  color: colorpalette.white,
+  baseSize: 16
 }
 
 export default {
@@ -26,7 +28,7 @@ export default {
       color: colorpalette.white,
       size: '16px'
     },
-    height: '60px',
+    height: '40px',
     padding: '10px'
   },
   palette: {
