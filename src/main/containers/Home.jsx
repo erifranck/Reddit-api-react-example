b2556d739a38c33e84df8268d639f8d7b9557a66
erifranck <erifranckjose@gmail.com>
2018-08-24 12:51:45 -0500

    render box with api information

--- src/main/containers/Home.jsx
+++ src/main/containers/Home.jsx
@@ -1,7 +1,12 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import {Button, Header} from 'common/styles'
-import Background from 'main/images/bg.png'
+import {Header} from 'common/styles'
+import {
+  Grid,
+  Row,
+  Col
+} from 'react-flexbox-grid'
+import {Box} from 'categories/styles'
 
 export class Home extends Component {
   constructor () {
@@ -14,9 +19,27 @@ export class Home extends Component {
     this.props.getSubreddit(this.state.reddit)
   }
   render () {
+    console.log(this.props.subreddits)
     return (
       <React.Fragment>
         <Header title={this.state.reddit}/>
+        <Grid>
+        <Row>
+          <Col md={10} sm={8}>
+            {
+              this.props.subreddits.map(({data}, index) => (
+                <Box
+                  key={index}
+                  author={data.author}
+                  url={data.url}
+                  title={data.title}
+                  image={data.thumbnail}
+                />
+              ))
+            }
+          </Col>
+        </Row>
+        </Grid>
       </React.Fragment>
     )
   }
@@ -24,5 +47,6 @@ export class Home extends Component {
 
 Home.propTypes = {
   className: PropTypes.string,
-  getSubreddit: PropTypes.func
+  getSubreddit: PropTypes.func,
+  subreddits: PropTypes.array
 }
