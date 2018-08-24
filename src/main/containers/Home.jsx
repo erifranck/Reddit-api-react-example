7d85e92207e39eae84c74fda6c460b6a98ee60c8
erifranck <erifranckjose@gmail.com>
2018-08-24 13:21:28 -0500

    divide the sponsor categories

--- src/main/containers/Home.jsx
+++ src/main/containers/Home.jsx
@@ -22,24 +22,43 @@ export class Home extends Component {
     console.log(this.props.subreddits)
     return (
       <React.Fragment>
-        <Header title={this.state.reddit}/>
-        <Grid>
-        <Row>
-          <Col md={10} sm={8}>
-            {
-              this.props.subreddits.map(({data}, index) => (
-                <Box
-                  key={index}
-                  author={data.author}
-                  url={data.url}
-                  title={data.title}
-                  image={data.thumbnail}
-                />
-              ))
-            }
-          </Col>
-        </Row>
-        </Grid>
+        <Header title={this.state.reddit} />
+        <div style={{marginTop: '20px'}}>
+          <Grid fluid>
+            <Row>
+              <Col md={10} sm={8}>
+                <div style={{marginBottom: '30px'}}>
+                  {
+                    this.props.subreddits.slice(0, 2).map(({data}, index) => (
+                      <Box
+                        key={index}
+                        author={data.author}
+                        url={data.url}
+                        title={data.title}
+                        votes={data.ups}
+                        image='//a.thumbs.redditmedia.com/dYcU_yJojWtlHVUaRsYQdC3bY7wTQ4pnvcxsRZtedP4.png'
+                      />
+                    ))
+                  }
+                </div>
+                <div>
+                  {
+                    this.props.subreddits.slice(2).map(({data}, index) => (
+                      <Box
+                        key={index}
+                        author={data.author}
+                        url={data.url}
+                        title={data.title}
+                        votes={data.ups}
+                        image={data.thumbnail}
+                      />
+                    ))
+                  }
+                </div>
+              </Col>
+            </Row>
+          </Grid>
+        </div>
       </React.Fragment>
     )
   }
